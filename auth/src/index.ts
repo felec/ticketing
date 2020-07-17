14ee774a355ae188ad49467791eea7c543f833c4
felec <felecrear@gmail.com>
2020-07-16 23:46:59 -0700

    user model, authentication

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -23,6 +24,21 @@ app.all('*', () => {
 
 app.use(errorHandler);
 
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log('connected to mongodb');
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 app.listen(3000, () => {
   console.log('Listening on 3000');
 });
+
+start();
