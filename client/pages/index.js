4c1a9056acca9677e6da078a6556314e909a8ff0
felec <felecrear@gmail.com>
2020-07-19 09:21:32 -0700

    configure axios request

---
+++ client/pages/index.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+  return <h1>Landing Page</h1>;
+};
+
+LandingPage.getInitialProps = async ({ req }) => {
+  if (typeof window === 'undefined') {
+    //on server
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: req.headers,
+      }
+    );
+    return data;
+  } else {
+    //on client
+    const { data } = await axios.get('/api/users/currentuser');
+    return data;
+  }
+};
+
+export default LandingPage;
